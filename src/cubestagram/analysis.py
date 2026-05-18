482b049381648268a501cfc018ec72af5ce86d25
Amrita Goswami <amrita16thaug646@gmail.com>
2026-05-18 16:12:04 +0200

    analysis: Accrue the density for all frames
    Also defines AnaylsisState that holds the number of cells in
    each dimension and the output list of densities (for all frames)
    
    Co-authored-by: Moritz Sallermann <moritzsallermann@gmail.com>

--- src/cubestagram/analysis.py
+++ src/cubestagram/analysis.py
@@ -1,9 +1,51 @@
-from cubestagram.scaffold import FrameInfo
+from cubestagram.scaffold import FrameInfo, partition_pos_into_cells
 from typing import Generator
 import numpy.typing as npt
-import numba
+from dataclasses import dataclass
+from cubestagram.util import decide_number_of_cells
 
 
-def analyze(frame_provider: Generator[FrameInfo]):
+@dataclass
+class Config:
+    target_lx: float
+    target_ly: float
+    target_lz: float
+
+
+@dataclass
+class AnalysisState:
+    nx: int | None = None
+    ny: int | None = None
+    nz: int | None = None
+    density_over_traj: list[npt.ArrayLike] | None = None
+
+
+def analyze(frame_provider: Generator[FrameInfo], config: Config):
+
+    # Initialize analysis state
+    state = AnalysisState()
+
     for frame_info in frame_provider:
-        ...
+        positions = frame_info.positions
+        box_lengths = frame_info.box_lengths
+
+        # Update state if the number of cells has not been set
+        if any(n is None for n in [state.nx, state.ny, state.nz]):
+            state.nx, state.ny, state.nz = decide_number_of_cells(
+                config=config, box_lengths=box_lengths
+            )
+            # Initialize the output list
+            state.density_over_traj = []
+
+        assert state.density_over_traj is not None
+
+        # Get the number density
+        density_per_frame = partition_pos_into_cells(
+            pos_frame=positions,
+            nx=state.nx,
+            ny=state.ny,
+            nz=state.nz,
+            box_lengths=box_lengths,
+        )
+
+        state.density_over_traj.append(density_per_frame)
